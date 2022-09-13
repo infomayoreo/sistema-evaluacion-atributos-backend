63320cd692bd341275690c27a994e8ff9d3cd647
Kevin Esaa <kesaa@intelix.biz>
2022-09-13 09:56:27 -0400

    feature: constant hash error codes

--- common/errorManager/AppCommonErrorCodes.ts
+++ common/errorManager/AppCommonErrorCodes.ts
@@ -2,20 +2,41 @@ import {CommonErrorDataContainer} from './CommonErrorDataContainer';
 
 let currentErrorNumber  : number = 1000;
 const WITHOUT_ERRORS = 0;
-const WITHOUT_ERRORS_NAME = "WITHOUT_ERRORS";
+const WITHOUT_ERRORS_NAME = "OK";
 const errorsSetNames = new Set<string>();
+const errorsHashCodeSet = new Set<number>();
 const errorMapNames = new Map<number, string>();
+const hashCodeOfNameMap = new Map<number, number>();
 
-const nextErrorNumber = (erroName :string) : number =>{
+const nextErrorNumber = (errorName :string) : number =>{
 
-    if(errorsSetNames.has(erroName)) {
-        throw new Error(`The error name is repeated: ${erroName}`);
+    if(errorsSetNames.has(errorName)) 
+    {
+        throw new Error(`The error name is repeated: ${errorName}`);
+    }
+    const hash = hashFromName(errorName);
+    if(errorsHashCodeSet.has(hash))
+    {
+        throw new Error(`The error hash is repeated. error: ${errorName}, hash: ${hash}`);
     }
     currentErrorNumber++;
-    errorMapNames.set(currentErrorNumber,erroName);
-    errorsSetNames.add(erroName);
+    errorsHashCodeSet.add(hash);
+    hashCodeOfNameMap.set(currentErrorNumber,hash);
+    errorMapNames.set(currentErrorNumber,errorName);
+    errorsSetNames.add(errorName);
     return currentErrorNumber;
 };
+
+const hashFromName = (errorName:string) : number => 
+{
+    let hash = 0;
+    for (let i = 0, len = errorName.length; i < len; i++) {
+        let chr = errorName.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
 
 const getErrorName = (errorCode:number) : string => {
     const errorName = errorMapNames.get(errorCode);
@@ -24,10 +45,13 @@ const getErrorName = (errorCode:number) : string => {
 
 const buildErrorDataContainer = (errorCode:number) : CommonErrorDataContainer => {
     const errorName = getErrorName(errorCode);
-    const errorDataContainer = new CommonErrorDataContainer(errorCode,errorName);
+    const hash = hashCodeOfNameMap.get(errorCode) ? hashCodeOfNameMap.get(errorCode) : hashCodeOfNameMap.get(commonErrorsCodes.UNKNOWN_ERROR);
+    const errorDataContainer = new CommonErrorDataContainer(hash ? hash : -1, errorName);
     return errorDataContainer;
 }
 
+errorsHashCodeSet.add(hashFromName(WITHOUT_ERRORS_NAME));
+hashCodeOfNameMap.set(WITHOUT_ERRORS,hashFromName(WITHOUT_ERRORS_NAME));
 errorMapNames.set(WITHOUT_ERRORS,WITHOUT_ERRORS_NAME);
 errorsSetNames.add(WITHOUT_ERRORS_NAME);
 
@@ -42,6 +66,9 @@ const commonErrorsCodes = {
     LIMIT_NOT_VALID_DATA_TYPE:nextErrorNumber("LIMIT_NOT_VALID_DATA_TYPE"),
 } as const;
 
+
+
 export {
-    nextErrorNumber, getErrorName, buildErrorDataContainer,WITHOUT_ERRORS, commonErrorsCodes
+    nextErrorNumber, getErrorName, buildErrorDataContainer, WITHOUT_ERRORS, commonErrorsCodes
 }
+
